73bff63d24ccdf3eefe8d0e354afd8f2e8ca723d
Al S <xee5ch.gh.al@il5.in>
2021-12-26 14:23:36 -0500

    Work around invalid GitHub API Meta Endpoint bad addrs.

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,16 +5,23 @@ terraform {
 locals {
   ipv4_regex = "\\d{1,3}.\\d{1,3}.\\d{1,3}.\\d{1,3}\\/\\d{2}"
   ips        = lookup(jsondecode(module.github_meta_api_ips.response), "pages", [])
-  github_pages_ipv4s = [
+  github_pages_ipv4s = sort([
     for ip in local.ips :
-    ip
-    if can(regex(local.ipv4_regex, ip))
-  ]
-  github_pages_ipv6s = [
+    replace(ip, "/32", "")
+    # The GitHub API Meta Endpoint returns multiple IP addresses for the Pages service
+    # that can be used for an apex domain and required A and AAAA records. See:
+    #
+    # https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site
+    #
+    # The API endpoint returns 192.30.x.y addresses that point to github.com, not 
+    # github.io and lead to TLS cert mistmatch failures. So filter those out as well.
+    if can(regex(local.ipv4_regex, ip)) && can(regex("185.*$", ip))
+  ])
+  github_pages_ipv6s = sort([
     for ip in local.ips :
-    ip
+    replace(ip, "/128", "")
     if !can(regex(local.ipv4_regex, ip))
-  ]
+  ])
 }
 
 module "github_meta_api_ips" {
@@ -47,4 +54,8 @@ module "dns_website_hosting" {
       ]
     }
   ]
+}
+
+module "aws_organization" {
+  source = "./aws/organizations/organization"
 }
